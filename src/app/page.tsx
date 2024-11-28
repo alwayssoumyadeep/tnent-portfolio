3e2cf50126d08012bf386097f53f75bb6dbbff5b
alwayssoumyadeep <ghosesoumyadeep1@gmail.com>
2024-11-28 17:29:27 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '@/sections/Navbar'
+import Hero from '@/sections/Hero'
+
+
+export default function page() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </>
+  )
+}
+
